3e3c9e4a77c210cadb5bb82618c53e3a357a5e70
SachinVarghese <smv@seldon.io>
2020-06-27 18:12:54 +0100

    updating build basepath

---
+++ docs/precache-manifest.697cea0c0da053444d207184692fe15e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cc8b98e6aba0422010435693a5c62d",
+    "url": "/citicern/index.html"
+  },
+  {
+    "revision": "a4254ef7430dd6fa610e",
+    "url": "/citicern/static/css/main.6c70d2ae.chunk.css"
+  },
+  {
+    "revision": "a62520c64c2b20dd5f46",
+    "url": "/citicern/static/js/2.8a183db2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/citicern/static/js/2.8a183db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4254ef7430dd6fa610e",
+    "url": "/citicern/static/js/main.e2af1408.chunk.js"
+  },
+  {
+    "revision": "032b75f5f4a87cd9d03b",
+    "url": "/citicern/static/js/runtime-main.828595fa.js"
+  },
+  {
+    "revision": "c3e7fea834548c6b7d7bf4ae3d371f72",
+    "url": "/citicern/static/media/user.c3e7fea8.png"
+  },
+  {
+    "revision": "34101b160c353908a3a831059d437b3b",
+    "url": "/citicern/static/media/water_sample.34101b16.png"
+  }
+]);
